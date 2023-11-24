08b8e2d256a420c24024becb8760b54aeb9f8026
niteshxp <niteshpatel8852@gmail.com>
2023-11-24 22:15:28 +0530

    moviesSlice setup done

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,11 +5,11 @@ export const USER_AVATAR =
     "https://occ-0-6247-2164.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABdpkabKqQAxyWzo6QW_ZnPz1IZLqlmNfK-t4L1VIeV1DY00JhLo_LMVFp936keDxj-V5UELAVJrU--iUUY2MaDxQSSO-0qw.png?r=e6e";
 
 export const API_OPTIONS = {
-    method: "GET",
+    method: 'GET',
     headers: {
-        accept: "application/json",
-        Authorization: "Bearer " + process.env.REACT_APP_TMDB_KEY,
-    },
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2ZTI1NDA4Njg0MzcyZjg4ZTdlNDUwNWVhNjU5YjZjNSIsInN1YiI6IjY1NjBjYTEwMzY3OWExMDk3NTI4MjJjMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.M6l4D1kfzI3HfYrUlUI8HHOZAw1-n5a2YtYGdXt4bbA'
+    }
 };
 
 export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w500";
