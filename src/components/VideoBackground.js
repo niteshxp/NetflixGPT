2829322444da0a64a2a19247240d14aed392a172
niteshxp <niteshpatel8852@gmail.com>
2023-11-25 23:46:12 +0530

    TMDB Integration done with main container

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -2,16 +2,15 @@ import { useSelector } from 'react-redux'
 import useMovieTrailer from '../hooks/useMovieTrailer'
 
 const VideoBackground = ({ movieId }) => {
-    
+
     const trailerVideo = useSelector(store => store.movies?.trailerVideo)
     useMovieTrailer(movieId);
 
     return (
-        <div>
+        <div className='w-screen'>
             <iframe
-                width="560"
-                height="315"
-                src={"https://www.youtube.com/embed/" + trailerVideo?.key} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                className='w-screen aspect-video'
+                src={"https://www.youtube.com/embed/" + trailerVideo?.key + "?&autoplay=1&mute=1"} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
             ></iframe>
         </div>
     )
